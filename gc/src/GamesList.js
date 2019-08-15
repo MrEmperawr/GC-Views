34c048cc3d276e9dac73d5649d6cadad25d3670d
Marcus <marcus.kalmar@gmail.com>
2019-08-15 16:15:53 +0200

    add GamesList and CompList components

---
+++ gc/src/GamesList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/GamesList.css';
+
+function GamesList() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default GamesList;
